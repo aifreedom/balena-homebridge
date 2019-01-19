0d2da8d33f914656e5dc4529b619a70f412b3c90
Randall Wood <297232+rhwood@users.noreply.github.com>
2019-01-19 17:01:50 -0500

    Retain data unless purged by balena APIs
    balena APIs purge data by removing the /data partion and recreating it, so no need to keep it here.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Constants
 WORKING_DIR="/usr/src/app"
 HOMEBRIDGE_DIR="/data/.homebridge/"
-KEEP_CONFIG=${KEEP_CONFIG:-no}
+# Use Homekit PIN from balenaCloud
 HOMEKIT_PIN=${HOMEKIT_PIN:-"000-00-000"}
 
 cd "$WORKING_DIR"
@@ -11,12 +12,7 @@ cd "$WORKING_DIR"
 if [ -d "${HOMEBRIDGE_DIR}" ]; then
   # Exists
   echo "Homebridge is already configured at \"${HOMEBRIDGE_DIR}\"."
-  if [ "${KEEP_CONFIG}" = "yes" ]; then
-    echo "KEEP_CONFIG=yes, Run \"Purge Data\" and then \"Reboot\" action to reset to default configuration."
-  else
-    echo "KEEP_CONFIG=no, Replacing ${HOMEBRIDGE_DIR}/config.json with .homebridge/config.json"
-    sed -e "s/333-33-333/${HOMEKIT_PIN}/g" .homebridge/config.json > "${HOMEBRIDGE_DIR}/config.json"
-  fi
+  echo "Run \"Purge Data\" and then \"Restart\" action to reset to default configuration."
 else
   # Does not exist
   echo "Homebridge is not configured."
@@ -24,5 +20,6 @@ else
   mkdir -p "$HOMEBRIDGE_DIR"
   echo "Copying Homebridge configuration"
   cp .homebridge/* "$HOMEBRIDGE_DIR"
+  # 333-33-333 is default pin, assume it has been changed to anything else
   sed -e "s/333-33-333/${HOMEKIT_PIN}/g" .homebridge/config.json > "${HOMEBRIDGE_DIR}/config.json"
 fi

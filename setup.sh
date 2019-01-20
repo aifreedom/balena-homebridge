46b20ad095e4e0538ef5ffbc1fc892ec8707b537
Randall Wood <297232+rhwood@users.noreply.github.com>
2019-01-20 10:00:34 -0500

    Allow configuration to be pulled from URL
    Closes #1 by allowing a GETable public URL to be used as the source for config.json.

--- setup.sh
+++ setup.sh
@@ -4,31 +4,41 @@
 WORKING_DIR="/usr/src/app"
 HOMEBRIDGE_DIR="/data/.homebridge/"
 CONFIG_JSON="${HOMEBRIDGE_DIR}/config.json"
-# Use Homekit PIN from balenaCloud
+# Use Homekit identity from balenaCloud
 HOMEKIT_PIN=${HOMEKIT_PIN:-"000-00-000"}
 HOMEKIT_USER=${HOMEKIT_USER:-"00:11:22:33:44:55"}
+# Retrieve config.json from the given URL
+CONFIG_URL=${CONFIG_URL:-""}
 # Keep config as specified in balenaCloud
 KEEP_CONFIG=${KEEP_CONFIG:-yes}
 
 cd "$WORKING_DIR"
 
 # Setup Homebridge
-if [ -d "${HOMEBRIDGE_DIR}" ]; then
-  # Exists
+if [ -d "${HOMEBRIDGE_DIR}" ] ; then
+  # Homebridge directory exists
   echo "Homebridge is already configured at \"${HOMEBRIDGE_DIR}\"."
-  if [ "${KEEP_CONFIG}" == "no" ]; then
+  if [ "${KEEP_CONFIG}" == "no" ] ; then
     echo "Replacing ${CONFIG_JSON} with default configuration."
     cp .homebridge/config.json ${CONFIG_JSON}
+    if [ -n "${CONFIG_URL}" ] ; then
+      curl -o ${CONFIG_JSON} "${CONFIG_URL}" \
+        || echo "Unable to download configuration from ${CONFIG_URL}"
+    fi
   else
     echo "Run \"Purge Data\" and then \"Restart\" action to reset to default configuration."
   fi
 else
-  # Does not exist
+  # Homebridge directory does not exist
   echo "Homebridge is not configured."
   echo "Creating Homebridge directory at \"${HOMEBRIDGE_DIR}\"."
   mkdir -p ${HOMEBRIDGE_DIR}
   echo "Copying Homebridge configuration"
   cp .homebridge/* ${HOMEBRIDGE_DIR}
+  if [ -n "${CONFIG_URL}" ] ; then
+    curl -o ${CONFIG_JSON} "${CONFIG_URL}" \
+      || echo "Unable to download configuration from ${CONFIG_URL}"
+  fi
 fi
 
 # Always update PIN and USERNAME to match balenaCloud
